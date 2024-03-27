10f2ea7487f803f26f07f3278ed5c17fae5eba00
koudai <786410175@qq.com>
2024-03-27 19:25:19 +0800

    alice server

---
+++ alice_server/ALC_Message.py
@@ -0,0 +1,8 @@
+
+import nltk 
+
+
+class overall_message(object):
+    def __init__(self, raw_msg):
+        self.raw_message = raw_msg
+
---
+++ alice_server/alice.py
@@ -0,0 +1,8 @@
+
+import ALC_Message
+
+
+# test
+if __name__ == '__main__':
+    print('this is test.')
+
